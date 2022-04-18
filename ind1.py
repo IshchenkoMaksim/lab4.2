36749e2d28c52f7c53fe0a3674b33244bcdb04a2
IshchenkoMaksim <ishchenko_ma21@mail.ru>
2022-04-18 11:58:12 +0300

    Добавлены задания

--- ind1.py
+++ ind1.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
+Выполнить индивидуальное задание 1 лабораторной работы 4.1, максимально
+задействовав имеющиеся в Python средства перегрузки операторов.
+
 Поле first — целое число, левая граница диапазона, включается в
 диапазон; поле second — целое число, правая граница диапазона,
 не включается в диапазон. Пара чисел представляет полуоткрытый
-интервал [first, second). Реализовать метод rangecheck() —
+интервал [first, second). Реализовать метод —
 проверку заданного целого числа на принадлежность диапазону
 """
 
@@ -17,18 +20,27 @@ class InRange:
         self.second = second
 
     def __lt__(self, other):
-        return other.first < self.first
+        if isinstance(other, InRange):
+            return other.first < self.first
+        else:
+            return False
 
     def __gt__(self, other):
-        return other.first > self.second
+        if isinstance(other, InRange):
+            return other.first > self.second
+        else:
+            return False
 
     def __eq__(self, other):
-        return self.first <= other.first < self.second
+        if isinstance(other, InRange):
+            return self.first <= other.first < self.second
+        else:
+            return False
 
 
 if __name__ == "__main__":
     interval = InRange(first=2, second=10)
-    x = InRange(first=5)
+    x = InRange(first=4)
     print(f"Число вне диапазона и больше него: {interval > x}")
     print(f"Число вне диапазона и меньше него: {interval < x}")
     print(f"Число в диапазоне: {interval == x}")
